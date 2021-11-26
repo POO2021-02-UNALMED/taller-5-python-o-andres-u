b210fb7407d39ad2cd6b0f86dd21a0e2ff318ae8
Oscar Andrés Úsuga Nanclares <oscarandresu@gmail.com>
2021-11-26 15:43:54 -0500

    Creational methods for Pez

--- zooAnimales/Pez.py
+++ zooAnimales/Pez.py
@@ -20,9 +20,15 @@ class Pez(Animal):
         return "nadar"
 
     @classmethod
-    def crearIguana(cls):
-        pass
+    def crearSalmon(cls, nombre, edad, genero):
+        cls.salmones += 1
+        salmon = Pez(colorEscamas="rojo", cantidadAletas=6, habitat="oceano", nombre=nombre, edad=edad, genero=genero)
+        cls._listado.append(salmon)
+        return salmon
 
     @classmethod
-    def crearSerpiente(cls):
-        pass
+    def crearBacalao(cls, nombre, edad, genero):
+        cls.bacalaos += 1
+        bacalao = Pez(colorEscamas="gris", cantidadAletas=6, habitat="oceano", nombre=nombre, edad=edad, genero=genero)
+        cls._listado.append(bacalao)
+        return bacalao
